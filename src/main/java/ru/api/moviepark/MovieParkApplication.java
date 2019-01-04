f68faa7ea1baa7b0d17f0ce15dc887d21859915e
mamutovrm1 <mamutovrm1@gmail.com>
2019-01-04 16:52:02 +0300

    packaging jar

--- src/main/java/ru/api/moviepark/MovieParkApplication.java
+++ src/main/java/ru/api/moviepark/MovieParkApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MovieParkApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MovieParkApplication.class, args);
 	}
