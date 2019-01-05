27cace095434b8c02176c639ddfc0b407dc7a936
mamutovrm1 <mamutovrm1@gmail.com>
2019-01-05 16:45:23 +0300

    json format edited.

--- src/main/java/ru/api/moviepark/controller/valueobjects/CreateSeanceInput.java
+++ src/main/java/ru/api/moviepark/controller/valueobjects/CreateSeanceInput.java
@@ -15,24 +15,10 @@ import java.time.LocalTime;
 @Getter
 @Builder
 public class CreateSeanceInput {
-
     LocalDate date;
-
-    @Column(name = "start_time")
     LocalTime startTime;
-
-    @Column(name = "end_time")
     LocalTime endTime;
-
-    @Column(name = "movie_id")
     Integer movieId;
-
-    @Column(name = "hall_id")
     Integer hallId;
-
-    @Column(name = "base_price")
     Integer basePrice;
-
-    @Column(name = "seance_table_exists")
-    Boolean seanceTableExists;
 }
