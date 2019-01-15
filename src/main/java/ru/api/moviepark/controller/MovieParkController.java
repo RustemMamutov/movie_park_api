e0bdaec1fd326509fe3246d8940400ac4b326e14
mamutovrm1 <mamutovrm1@gmail.com>
2019-01-15 23:52:49 +0300

    readme fixed.

--- src/main/java/ru/api/moviepark/controller/MovieParkController.java
+++ src/main/java/ru/api/moviepark/controller/MovieParkController.java
@@ -1,7 +1,6 @@
 package ru.api.moviepark.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import ru.api.moviepark.controller.valueobjects.BlockPlaceInput;
