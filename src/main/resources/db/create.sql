0b76e604eba90b1c573404f866bc575824e3f7f1
Rustem Mamutov <mamutovrm1@gmail.com>
2019-09-24 22:16:27 +0300

    Test db configured.

--- src/main/resources/db/create.sql
+++ src/main/resources/db/create.sql
@@ -1,63 +1,59 @@
-CREATE SCHEMA IF NOT EXISTS cinema_park;
+CREATE SCHEMA IF NOT EXISTS movie_park_develop;
 
-CREATE TABLE IF NOT EXISTS movie_park.halls
-(
- hall_id int4 NOT NULL,
- hall_row int4 NOT NULL,
- place int4 NOT NULL,
- is_vip boolean NOT NULL,
- PRIMARY KEY (hall_id, hall_row, place)
+CREATE TABLE movie_park_develop.halls (
+    hall_id int4 NOT NULL,
+    place_id int4 NOT NULL,
+    is_vip bool NOT NULL,
+    PRIMARY KEY (hall_id, place_id)
 );
 
-CREATE TABLE IF NOT EXISTS movie_park.main_schedule
-(
- seance_id int4 NOT NULL,
- seance_date date NOT NULL,
- start_time time NOT NULL,
- end_time time NOT NULL,
- movie_id int4 NOT NULL,
- hall_id int4 NOT NULL,
- base_price int4 NOT null,
- PRIMARY KEY (seance_id)
+CREATE TABLE movie_park_develop.movies (
+    id int4 NOT NULL,
+    name VARCHAR NOT NULL,
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS movie_park.movies
-(
- movie_id int4 NOT NULL,
- movie_name text NOT NULL,
- CONSTRAINT movies_pkey PRIMARY KEY (movie_id)
+CREATE TABLE movie_park_develop.movie_parks (
+    id int4 NOT NULL,
+    "name" text NOT NULL,
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS movie_park.prices_delta
-(
- start_time time NOT NULL,
- end_time time NOT NULL,
- period_start_date date NOT NULL,
- period_end_date date NOT NULL,
- is_vip bool NOT NULL,
- price_delta int4 NOT NULL
+CREATE TABLE movie_park_develop.main_schedule (
+    seance_id int4 NOT NULL,
+    seance_date date NOT NULL,
+    start_time time NOT NULL,
+    end_time time NOT NULL,
+    movie_park_id int4 NOT NULL,
+    movie_id int4 NOT NULL,
+    hall_id int4 NOT NULL,
+    base_price int4 NOT NULL,
+    vip_price int4 NOT NULL,
+    PRIMARY KEY (seance_id),
+    FOREIGN KEY (movie_park_id) REFERENCES movie_park_develop.movie_parks(id),
+    FOREIGN KEY (movie_id) REFERENCES movie_park_develop.movies(id)
 );
 
-CREATE TABLE IF NOT EXISTS movie_park.seances_places
-(
- seance_id int4 NOT NULL,
- hall_id int4 NOT NULL,
- hall_row int4 NOT NULL,
- place int4 NOT NULL,
- is_vip bool NOT NULL,
- price int4 NOT NULL,
- "blocked" bool NOT NULL,
- PRIMARY KEY (seance_id, hall_row, place)
+CREATE TABLE movie_park_develop.seances_places (
+    seance_id int4 NOT NULL,
+    place_id int4 NOT NULL,
+    "blocked" bool NOT NULL,
+    PRIMARY KEY (seance_id, place_id)
 );
 
-create or replace view
- movie_park.main_schedule_view as
-select main_schedule.seance_id,
- main_schedule.seance_date,
- main_schedule.start_time,
- main_schedule.end_time,
- movies.movie_name,
- main_schedule.hall_id
-from movie_park.main_schedule
- inner join movie_park.movies
- on main_schedule.movie_id = movies.movie_id;
+CREATE OR REPLACE VIEW movie_park_develop.main_schedule_view
+AS SELECT
+    main_schedule.seance_id,
+    main_schedule.seance_date,
+    main_schedule.start_time,
+    main_schedule.end_time,
+    movie_parks.id AS movie_park_id,
+    movie_parks."name" AS movie_park_name,
+    movies.id AS movie_id,
+    movies."name" AS movie_name,
+    main_schedule.hall_id,
+    main_schedule.base_price,
+    main_schedule.vip_price
+FROM movie_park_develop.main_schedule
+JOIN movie_park_develop.movie_parks ON main_schedule.movie_park_id = movie_parks.id
+JOIN movie_park_develop.movies ON main_schedule.movie_id = movies.id;
--- src/main/resources/db/queries.sql
+++ src/main/resources/db/queries.sql
@@ -1,29 +1,19 @@
-CREATE OR REPLACE FUNCTION cinema_park.create_new_seances(days integer)
-    RETURNS void
-    LANGUAGE sql
-AS
-$function$
-INSERT into cinema_park.seances_places
+CREATE OR REPLACE FUNCTION movie_park_develop.create_new_seances(days integer)
+ RETURNS void
+ LANGUAGE sql
+AS $function$
+INSERT into movie_park_develop.seances_places
     (
         select main_schedule.seance_id,
-               main_schedule.hall_id,
-               halls.hall_row,
-               halls.place,
-               halls.is_vip,
-               main_schedule.base_price + prices_delta.price_delta as price,
-               false                                               as is_blocked
-        from cinema_park.main_schedule
-                 inner join cinema_park.halls
-                            on main_schedule.hall_id = halls.hall_id
-                 inner join cinema_park.prices_delta
-                            on prices_delta.period_start_date < main_schedule.seance_date
-                                and main_schedule.seance_date <= prices_delta.period_end_date
-                                and halls.is_vip = prices_delta.is_vip
-                                and prices_delta.start_time <= main_schedule.start_time
-                                and main_schedule.start_time <= prices_delta.end_time
+               halls.place_id,
+               false as "blocked"
+        from movie_park_develop.main_schedule
+	         inner join movie_park_develop.halls
+                on main_schedule.hall_id = halls.hall_id
         where main_schedule.seance_date <= (select CURRENT_DATE + days * INTERVAL '1 day')
+        order by seance_id, place_id
     )
-ON conflict (seance_id, hall_row, place)
+ON conflict (seance_id, place_id)
     DO nothing
 $function$
 ;
