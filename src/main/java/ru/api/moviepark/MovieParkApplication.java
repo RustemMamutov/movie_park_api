2b2da336dac2f025ba20abea8cff00ed2635e0b7
Rustem Mamutov <mamutovrm1@gmail.com>
2020-01-19 20:01:15 +0300

    supports args with properties for db connection.

--- src/main/java/ru/api/moviepark/MovieParkApplication.java
+++ src/main/java/ru/api/moviepark/MovieParkApplication.java
@@ -6,6 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MovieParkApplication {
     public static void main(String[] args) {
-        SpringApplication.run(MovieParkApplication.class, args);
+        if (args.length == 3) {
+            System.setProperty("spring.datasource.url", args[0]);
+            System.setProperty("spring.datasource.username", args[1]);
+            System.setProperty("spring.datasource.password", args[2]);
+        }
+
+        SpringApplication application = new SpringApplication(MovieParkApplication.class);
+        application.setAddCommandLineProperties(false);
+        application.run(args);
     }
 }
